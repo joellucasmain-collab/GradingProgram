33ec3b126eed81b596938fdf5819317897d95f82
40205940 <40205940@HY-R104-10.resource.uc>
2025-12-02 12:19:26 +0000

    First commit

---
+++ src/main/java/com/mycompany/gradingproject/Gradingproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradingproject;
+
+/**
+ *
+ * @author 40205940
+ */
+public class Gradingproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
